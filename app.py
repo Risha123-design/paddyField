9e0a9246427239316bbf3ed468defcd595acb36a
Risha123-design <shaaniqibrahim21@gmail.com>
2025-07-29 14:41:10 +0530

    Merge remote main with local changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is your Paddy AI Flask project!"
+    return render_template("index.html")  # Make sure you create this file
 
 if __name__ == "__main__":
     app.run(debug=True)
